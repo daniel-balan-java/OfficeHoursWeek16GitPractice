31e1b4a15b94d9101ef4bc264321bebcc32741f0
danielbalan <daniel.balan1995@gmail.com>
2023-01-11 13:26:27 -0500

    Revert "Created test steps changes"
    This reverts commit ba1e7d300cf3af9bea6213b1506c228150387124.

--- src/cydeo/TestCase004.java
+++ src/cydeo/TestCase004.java
@@ -3,6 +3,5 @@ package cydeo;
 public class TestCase004 {
     public static void main(String[] args) {
         System.out.println("Created automation steps for my ticket");
-        System.out.println("Created changes");
     }
 }
